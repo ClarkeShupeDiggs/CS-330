a9bbdeaee0fbe27548d6a5d37ab55f065ad02e76
Clarke Shupe-Diggs <ClarkeShupeDiggs@users.noreply.github.com>
2016-02-13 10:09:30 -0500

    Create boolean_values.c

---
+++ Examples/boolean_values.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int false = 0;
+	int true = 1;
+	int num = 5;
+	int match;
+	if(num==2){
+		match = true;
+		printf("true");
+	}else{
+		match = false;
+		printf("false");
+	}
+}
